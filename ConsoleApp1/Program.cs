5058ace419410fed56a5759883a1b07c8be27b20
felipe jimenez sanchez <felipe>
2026-03-12 18:14:51 -0500

    menu Principal

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+/// MENU PRINCIPAL
+        Console.Clear();
+        Console.WriteLine("1. Libro");
+        Console.WriteLine("2. Usuario");
+        Console.WriteLine("3. Prestamos");
+        Console.WriteLine("4. Busquedas y Reportes");
+        Console.WriteLine("5. Guardar / Cargar datos");
+        Console.WriteLine("6. Salir");
     }
 }
